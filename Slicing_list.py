ec4ace18bcf879818d43fd0f8d4f7ca39f96723e
Kanika Rawat <114948582+Kanika-Rawat121@users.noreply.github.com>
2023-02-28 13:53:17 +0530

    Add files via upload
    Enter the nested list:12wedfr56y7u8i9ij
    Nested list 12wedfr56y7u8i9ij
    Elements of list 1
    Elements of list 2
    Elements of list w
    Elements of list e
    Elements of list d
    Elements of list f
    Elements of list r
    Elements of list 5
    Elements of list 6
    Elements of list y
    Elements of list 7
    Elements of list u
    Elements of list 8
    Elements of list i
    Elements of list 9
    Elements of list i
    Elements of list j

---
+++ Slicing_list.py
@@ -0,0 +1,9 @@
+"""WAP to display the values for nested list."""
+#Inputting elements in list
+l=input("Enter the nested list:")
+print("Nested list",l)
+#Slicing elements in list to display
+for i in l:
+    print("Elements of list",i)
+
+
