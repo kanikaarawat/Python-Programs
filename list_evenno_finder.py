fb42aa10a4489083c3d045e21ed3115ac2ce4f06
Kanika Rawat <114948582+Kanika-Rawat121@users.noreply.github.com>
2023-02-28 13:46:13 +0530

    Add files via upload
    Enter a list element separated by space:12 1322 21 33443 65 44 5898
    12 1322 44 5898 
    The sum of numbers: 7276

---
+++ list_evenno_finder.py
@@ -0,0 +1,14 @@
+"""WAP to print odd numbers and sum of odd numbers from the list."""
+#Inputting elements separated by space so that we can split them into list
+input_string = input("Enter a list element separated by space:")
+
+#Using split function
+l1  = input_string.split()
+sum1=0
+
+#Even numbers are found int the list
+for num in l1:
+    if int(num)%2==0:
+      sum1+=int(num)  
+      print(num,end=" ")    
+print("\nThe sum of numbers:",sum1)
