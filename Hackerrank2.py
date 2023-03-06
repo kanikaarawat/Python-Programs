15db26513a829f24d59e5a055daf9a14020dc43f
Kanika Rawat <114948582+Kanika-Rawat121@users.noreply.github.com>
2023-03-06 11:05:53 +0530

    Create Hackerrank2.py
    Input (stdin)
    3
    2
    Your Output (stdout)
    5
    1
    6
    Expected Output
    5
    1
    6

---
+++ Hackerrank2.py
@@ -0,0 +1,22 @@
+"""Task
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+
+Input Format
+
+The first line contains the first integer, a.
+The second line contains the second integer, b.
+
+Constraints
+
+1<=a,b<=10^10"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+print(a+b)
+print(a-b)
+print(a*b)
