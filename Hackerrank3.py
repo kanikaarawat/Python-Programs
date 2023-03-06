c1a5b27153b55288b9b2dcf051e30516f79be3a9
Kanika Rawat <114948582+Kanika-Rawat121@users.noreply.github.com>
2023-03-06 11:28:38 +0530

    Create Hackerrank3.py
    Input (stdin)
    5
    Your Output (stdout)
    0
    1
    4
    9
    16
    Expected Output
    0
    1
    4
    9
    16

---
+++ Hackerrank3.py
@@ -0,0 +1,10 @@
+"""Task
+The provided code stub reads and integer, n , from STDIN. For all non-negative integers , i<n, print i^2 .
+Output Format
+Print  lines,n one corresponding to each i ."""
+
+if __name__ == '__main__':
+    n = int(input())
+for i in range(0,n):
+    print(i**2)
+  
