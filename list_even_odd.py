c0c3cda8d5a55c613cc3a1fe60472393427aac00
Kanika Rawat <114948582+Kanika-Rawat121@users.noreply.github.com>
2023-03-10 19:45:54 +0530

    Add files via upload
    Enter number of elements:5
    Enter element:123
    Enter element:987
    Enter element:5678
    Enter element:456
    Enter element:456789
    The even list [5678, 456]
    The odd list [123, 987, 456789]

---
+++ list_even_odd.py
@@ -0,0 +1,15 @@
+"""WAP that takes a list ‘L’ as an argument , adds 5 in all the odd values and 10 in all the even values of the list l. Also display the list."""
+a=[]
+n=int(input("Enter number of elements:"))
+for i in range(1,n+1):
+    b=int(input("Enter element:"))
+    a.append(b)
+even=[]
+odd=[]
+for j in a:
+    if(j%2==0):
+       even.append(j)
+    else:
+       odd.append(j)
+print("The even list",even)
+print("The odd list",odd)
