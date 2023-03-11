51a5fc4daa2c0b39593295ff3ed3daadcf72ab7e
Kanika Rawat <114948582+Kanika-Rawat121@users.noreply.github.com>
2023-03-11 22:50:15 +0530

    Update third_side_triangle.py

--- third_side_triangle.py
+++ third_side_triangle.py
@@ -1,3 +1,4 @@
 """ Write a Python program to get the third side of a right-angled triangle from two given sides. """
 side1=int(input("Enter the side1:"))
-side2=int(input("Enter the side2:"))
+side2=int(input("Enter the side2:")
+side3
