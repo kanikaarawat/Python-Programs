f26cce31ebbde42cd324c10a4ed5355c0eb93b6f
Kanika Rawat <114948582+Kanika-Rawat121@users.noreply.github.com>
2023-03-13 16:07:44 +0530

    Update List.py

--- List.py
+++ List.py
@@ -1,1 +1,9 @@
-wrong file just for streaks
+def Convert_dict(a):  
+    init = iter(list1)  
+    res_dct = dict(zip(init, init))  
+    return res_dct  
+  
+  
+# Driver code  
+list1 = ['x', 1, 'y', 2, 'z', 3]  
+print(Convert_dict(list1))  
